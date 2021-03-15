6eae461be3b1823b064af9f1abb9eebef7bae878
Vegely <bogurad.baranski@alumnos.upm.es>
2021-03-15 22:36:28 +0100

    Escritura de un inicio básico para cualquier programa en SDL

--- Damas codigo/SDLPrueba.c
+++ Damas codigo/SDLPrueba.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
 
+//Al usar la librería SDL es importante que el main quede definido de la siguiente forma para que pueda ser portable a todo SO.
+//De momento es un archivo de prueba en un futuro se convertirá en función.
+int main(int argv, char** args)
+{
+
+    //Se declara la ventana donde se renderizara y la superficie contenida por la ventana
+
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+
+        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+            {
+                printf( "SDL no pudo iniciarse: %s\n", SDL_GetError() );
+            }
+
+        else
+            {
+
+        //Se crea una ventana centrada con dimensiones 400 x 400 y con el último argumento muestra la ventana
+
+                window = SDL_CreateWindow( "nombre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_SHOWN );
+
+                    if( window == NULL )
+                        {
+                            printf( "No se pudo crear la ventana: %s\n", SDL_GetError() );
+                        }
+
+
+
+    return 0;
+}
