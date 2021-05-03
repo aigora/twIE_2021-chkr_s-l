3d0a9668f9d5de28ad716b40066385dc77d358bb
Vegely <bogurad.baranski@alumnos.upm.es>
2021-05-03 16:30:37 +0200

    Actualizada la opción de poner el tablero en pantalla
    Aun queda quitar que las variables sean globales y sacar las funciones al segundo archivo

--- Damas Codigo/Funciones.c
+++ Damas Codigo/Funciones.c
@@ -2,51 +2,74 @@
 #include <SDL.h>
 #include <stdbool.h>
 
-//Al usar la librería SDL es importante que el main quede definido de la siguiente forma para que pueda ser portable a todo SO.
-//De momento es un archivo de prueba en un futuro se convertirá en función.
-bool iniciar()
-{
-
-    //Se declara la ventana donde se renderizara y la superficie contenida por la ventana
-
-    SDL_Window* Ventana= NULL;
-    SDL_Surface* Superficepantalla = NULL;
-    bool funciona = true;
-
-        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-            {
-                printf( "SDL no pudo iniciarse: %s\n", SDL_GetError() );
-                funciona = false;
-            }
-
-        else
-            {
-
-        //Se crea una ventana centrada con dimensiones 400 x 400 y con el último argumento muestra la ventana
-
-                Ventana = SDL_CreateWindow( "nombre", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_SHOWN );
-
-                    if( Ventana == NULL )
-                        {
-                            printf( "No se pudo crear la ventana: %s\n", SDL_GetError() );
-                            funciona = false;
-
-                        }
-                    else
-                        {
-                            //Se coge la superficie de la ventana para poder colocar elementos en ella
-                            Superficepantalla = SDL_GetWindowSurface( Ventana );
-
-                            //Rellena la superficie de blanco
-                            SDL_FillRect( Superficepantalla, NULL, SDL_MapRGB( Superficepantalla->format, 0xFF, 0xFF, 0xFF ) );
-
-                            //Actualiza la superficie
-                            SDL_UpdateWindowSurface( Ventana );
-
-                            //Espera 2000 milisegundos para que no desaparezca inmediatamente
-                            SDL_Delay( 2000 );
-                        }
-            }
-    return funciona;
-}
-
+//SDL_Window* Ventana= NULL;
+//SDL_Surface* Superficepantalla = NULL;
+//SDL_Surface* Tablero = NULL;
+//
+//
+////Al usar la librería SDL es importante que el main quede definido de la siguiente forma para que pueda ser portable a todo SO.
+////De momento es un archivo de prueba en un futuro se convertirá en función.
+//bool iniciar()
+//{
+//
+//    //Se declara la ventana donde se renderizara y la superficie contenida por la ventana
+//
+//
+//    bool funciona = true;
+//
+//        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+//            {
+//                printf( "SDL no pudo iniciarse: %s\n", SDL_GetError() );
+//                funciona = false;
+//            }
+//
+//        else
+//            {
+//
+//        //Se crea una ventana centrada con dimensiones 1080 x 1920 y con el último argumento muestra la ventana
+//
+//                Ventana = SDL_CreateWindow( "Damas.exe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN );
+//
+//                    if( Ventana == NULL )
+//                        {
+//                            printf( "No se pudo crear la ventana: %s\n", SDL_GetError() );
+//                            funciona = false;
+//
+//                        }
+//                    else
+//                        {
+//                            //Se coge la superficie de la ventana para poder colocar elementos en ella
+//                            Superficepantalla = SDL_GetWindowSurface( Ventana );
+//                        }
+//            }
+//    return funciona;
+//}
+//
+//bool cargarImagen()
+//{
+//    bool funciona = true;
+//
+//    Tablero=SDL_LoadBMP("chekers_blue.bmp");
+//
+//        if (Tablero == NULL)
+//        {
+//            printf("No se pudo iniciar la imagen %s. Error de SDL %s \n ","chekers_blue.bmp",SDL_GetError());
+//            funciona = false;
+//        }
+//
+// return funciona;
+//}
+//
+//void cerrar()
+//{
+//
+//    SDL_FreeSurface( Tablero );
+//    Tablero = NULL;
+//
+//
+//    SDL_DestroyWindow( Ventana );
+//    Ventana = NULL;
+//
+//    //Cerrar SDL
+//    SDL_Quit();
+//}
