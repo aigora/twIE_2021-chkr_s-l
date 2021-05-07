b920e45721fa437e2520660e6d93679cbc524eb1
Vegely <bogurad.baranski@alumnos.upm.es>
2021-05-07 13:03:10 +0200

    Creada estructura para almacenar el tamaño de las casillas

--- Damas Codigo/Funciones.c
+++ Damas Codigo/Funciones.c
@@ -12,99 +12,88 @@ typedef struct
 
 
 
-void fondo (SDL_Window *Ventana,SDL_Surface *Superficepantalla, SDL_Surface *Tablero,SDL_Renderer *Render, SDL_Texture *Textura)
+
+void fondo (SDL_Window *Ventana,SDL_Renderer *Render, SDL_Texture *Textura)
 {
+    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+        {
+            printf( "SDL no pudo iniciarse: %s\n", SDL_GetError() );
+        }
+    else
+        {
 
+           if(!SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ))
+           {
+               printf("Aviso: Filtración linear de texturas no disponible");
+           }
+            Ventana = SDL_CreateWindow( "Damas.exe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN );
 
-
-            if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
-            {
-                printf( "SDL no pudo iniciarse: %s\n", SDL_GetError() );
-
-            }
-
-        else
-            {
-
-               if(!SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ))
-               {
-                   printf("Aviso: Filtración linear de texturas no disponible");
-               }
-
-
-                Ventana = SDL_CreateWindow( "Damas.exe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN );
-
-                    if( Ventana == NULL )
-                        {
-                            printf( "No se pudo crear la ventana: %s\n", SDL_GetError() );
-
-
-                        }
-                    else
-                        {
-
-                            Render= SDL_CreateRenderer( Ventana, -1, SDL_RENDERER_ACCELERATED );
-
-
-                            if( Render == NULL )
-                                    {
-                                        printf( "El render no se pudo crear SDL Error: %s\n", SDL_GetError() );
-                                    }
-                            else
-                                    {
-                                        SDL_SetRenderDrawColor( Render, 0xFF, 0xFF, 0xFF, 0xFF );
-
-
-                                        SDL_Texture *newTexture = NULL;
-                                    	SDL_Surface *SupCarg = SDL_LoadBMP("chekers_blue.bmp");
-                                                            if( SupCarg == NULL )
-                                                            {
-                                                                printf( "No se pudo cargar la imagen: chekers_blue.bmp SDL Error: %s\n", SDL_GetError() );
-                                                            }
-                                                            else
-                                                            {
-
-                                                                newTexture = SDL_CreateTextureFromSurface( Render, SupCarg );
-                                                                if( newTexture == NULL )
-                                                                {
-                                                                    printf( "No se pudo cargar crear la textura SDL Error: %s\n", SDL_GetError() );
-                                                                }
-
-
-                                                                SDL_FreeSurface( SupCarg );
-                                                                Textura=newTexture;
-                                                                  if (Textura == NULL)
-                                                                    {
-                                                                        printf("Fallo al cargar las texturas\n");
-
-                                                                    }
-                                                                else
-                                                                {
-                                                                    SDL_RenderClear( Render );
-
-
-                                                                    SDL_RenderCopy( Render, Textura, NULL, NULL );
-
-
-                                                                    SDL_RenderPresent( Render );
-                                                                }
-                                                            }
-
+                if( Ventana == NULL )
+                    {
+                        printf( "No se pudo crear la ventana: %s\n", SDL_GetError() );
+                    }
+                else
+                    {
+                        Render= SDL_CreateRenderer( Ventana, -1, SDL_RENDERER_ACCELERATED );
+                        if( Render == NULL )
+                                {
+                                    printf( "El render no se pudo crear SDL Error: %s\n", SDL_GetError() );
+                                }
+                        else
+                                {
+                                    SDL_SetRenderDrawColor( Render, 0xFF, 0xFF, 0xFF, 0xFF );
+                                    SDL_Texture *newTexture = NULL;
+                                    SDL_Surface *SupCarg = SDL_LoadBMP("chekers_blue.bmp");
+                                        if( SupCarg == NULL )
+                                        {
+                                            printf( "No se pudo cargar la imagen: chekers_blue.bmp SDL Error: %s\n", SDL_GetError() );
                                         }
-                        }
+                                        else
+                                        {
+                                            newTexture = SDL_CreateTextureFromSurface( Render, SupCarg );
+                                            if( newTexture == NULL )
+                                            {
+                                                printf( "No se pudo cargar crear la textura SDL Error: %s\n", SDL_GetError() );
+                                            }
+                                            SDL_FreeSurface( SupCarg );
+                                            Textura=newTexture;
+                                              if (Textura == NULL)
+                                                {
+                                                    printf("Fallo al cargar las texturas\n");
+                                                }
+                                            else
+                                            {
+                                                SDL_RenderClear( Render );
+                                                SDL_RenderCopy( Render, Textura, NULL, NULL );
+                                                SDL_RenderPresent( Render );
 
-            }
+                                                SDL_Rect fillRect = { 1920 / 4, 1080 / 4, 1920 / 2, 1080 / 2 };
+                                                SDL_SetRenderDrawColor( Render, 0xFF, 0x00, 0x00, 0xFF );
+                                                SDL_RenderFillRect( Render, &fillRect );
+
+                                                	SDL_RenderPresent( Render );
+                                            }
+                                        }
+
+                                }
+                    }
+
+        }
 }
 
-/*void Pintar(int Tipo_ficha [32], int numero_casilla, bool rodear, bool rodeados[32],SDL_Window *Ventana,SDL_Surface *Superficepantalla, SDL_Surface *Tablero)
+/*void Pintar(int Tipo_ficha [32], int numero_casilla, bool rodear, bool rodeados[32],SDL_Renderer *Render)
 {
 
     if(rodear==true)
         {
-            if(Tipo_ficha[numero_casilla]==2)
-                //dibujar circulo verde
-            else
-                //dibujar círculo naranja
+            if(rodeados[numero_casilla]==false)
+                {
+                    if(Tipo_ficha[numero_casilla]==2)
+                    //dibujar circulo verde
+                    else
+                    //dibujar círculo naranja
+                }
+
         }
     else
         {
@@ -136,8 +125,13 @@ void fondo (SDL_Window *Ventana,SDL_Surface *Superficepantalla, SDL_Surface *Tab
 
                 }
         }
-}*/
+}
 
+void despintar(int Tipo_ficha [32], int numero_casilla,SDL_Renderer *Render)
+{
+
+}
+*/
 punto pos_raton ()
 {
 
@@ -173,16 +167,16 @@ punto pos_raton ()
    return click;
 }
 
-void cerrar (SDL_Window *Ventana,SDL_Surface *Superficepantalla, SDL_Surface *Tablero, SDL_Texture *Textura, SDL_Renderer *Render)
-            {
-                                    SDL_DestroyTexture( Textura );
-                                    Textura = NULL;
+void cerrar (SDL_Window *Ventana, SDL_Texture *Textura, SDL_Renderer *Render)
+{
+        SDL_DestroyTexture( Textura );
+        Textura = NULL;
 
 
-                                    SDL_DestroyRenderer( Render );
-                                    SDL_DestroyWindow( Ventana );
-                                    Ventana = NULL;
-                                    Render = NULL;
+        SDL_DestroyRenderer( Render );
+        SDL_DestroyWindow( Ventana );
+        Ventana = NULL;
+        Render = NULL;
 
-                                    SDL_Quit();
-            }
+        SDL_Quit();
+}
--- Damas Codigo/Main.c
+++ Damas Codigo/Main.c
@@ -2,26 +2,61 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef struct
+{
+    int x1,y1,x2,y2;
+}Cuadrante;
+
 
 
 
 int main(int argv, char** args)
 {
             SDL_Window *Ventana= NULL;
-            SDL_Surface *Superficepantalla = NULL;
-            SDL_Surface *Tablero = NULL;
             SDL_Renderer *Render = NULL;
             SDL_Texture *Textura = NULL;
 
+            const Cuadrante  dimensiones_casilla [32]={
+                                    {585,72,710,189}, //Primera fila
+                                    {835,72,960,189},
+                                    {1085,72,1210,189},
+                                    {1335,72,1460,189},
+                                    {460,189,585,306}, //Segunda fila
+                                    {710,189,835,306},
+                                    {900,189,1085,306},
+                                    {1210,189,1335,306},
+                                    {585,306,710,423}, //Tercera fila
+                                    {835,306,960,423},
+                                    {1085,306,1210,423},
+                                    {1335,306,1460,423},
+                                    {460,423,585,540}, //Cuarta fila
+                                    {710,423,835,540},
+                                    {900,423,1085,540},
+                                    {1210,423,1335,540},
+                                    {585,540,710,657}, //Quinta fila
+                                    {835,540,960,657},
+                                    {1085,540,1210,657},
+                                    {1335,540,1460,657},
+                                    {460,657,585,774}, //Sexta fila
+                                    {710,657,835,774},
+                                    {900,657,1085,774},
+                                    {1210,657,1335,774},
+                                    {585,774,710,891}, //Séptima fila
+                                    {835,774,960,891},
+                                    {1085,774,1210,891},
+                                    {1335,774,1460,891},
+                                    {460,891,585,1008}, //Octava fila
+                                    {710,891,835,1008},
+                                    {900,891,1085,1008},
+                                    {1210,891,1335,1008}
+                                };
 
 
-
-
-fondo (Ventana,Superficepantalla, Tablero,Render, Textura);
+fondo (Ventana,Render, Textura);
 
         pos_raton ();
 
-    cerrar (Ventana,Superficepantalla, Tablero, Textura, Render);
+cerrar (Ventana, Textura, Render);
 
 //    int opcion[5];
 //    double tiempo=0;
